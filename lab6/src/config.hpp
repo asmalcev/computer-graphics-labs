e3d2cdfe0d43f621f3438b5de0b21e9c206599e0
alex <qzerrty@protonmail.com>
2021-11-21 23:45:35 +0300

    lab6 done

--- lab6/src/config.hpp
+++ lab6/src/config.hpp
@@ -1,4 +1,5 @@
 #include <cstddef>
+#include <iostream>
 
 constexpr size_t windowWidth = 640;
 constexpr size_t windowHeight = 640;
--- lab6/src/main.cpp
+++ lab6/src/main.cpp
@@ -4,6 +4,17 @@
 
 bool keys[256];
 
+constexpr float LEFT = -1, RIGHT = 1, DOWN = -1, UP = 1;
+constexpr float ambient[] = {0.3, 0.6, 0.2, 1};
+float coords[] = {0, 0, 0};
+
+constexpr float light_pos1[] = {0, 0, -1, 0}, light_pos2[] = {1, 0, 0, 1};
+
+constexpr float color[4] = {0, 1, 0, 1};
+constexpr float shine[1] = {39};
+
+GLUquadricObj * quadricObj;
+
 GLvoid resizeScene(GLsizei w, GLsizei h)
 {
 	if (h == 0) {
@@ -15,45 +26,73 @@ GLvoid resizeScene(GLsizei w, GLsizei h)
 
 GLvoid renderScene(GLvoid)
 {
-	glClear(GL_COLOR_BUFFER_BIT);
-	glBegin(GL_POLYGON);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glVertex3f(0.5, 0.0, 0.5);
-	glVertex3f(0.5, 0.0, 0.0);
-	glVertex3f(0.0, 0.5, 0.0);
-	glVertex3f(0.0, 0.0, 0.5);
+	glPushMatrix();
 
-	glEnd();
-	glFlush();
+	glTranslatef(coords[0], coords[1], coords[2]);
+
+	glLightfv(GL_LIGHT0, GL_POSITION, light_pos1);
+	glLightfv(GL_LIGHT0, GL_POSITION, light_pos2);
+
+	quadricObj = gluNewQuadric();
+		glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, color);
+		gluSphere(quadricObj, 1, 20, 20);
+
+		glTranslatef(-15, 9, 2.2);
+		glMaterialfv(GL_FRONT_AND_BACK, GL_SHININESS, shine);
+		gluSphere(quadricObj, 6, 10, 10);
+	gluDeleteQuadric(quadricObj);
+
+	glPopMatrix();
+
+	glutSwapBuffers();
 }
 
 GLvoid processNormalKeys(unsigned char key, int x, int y) {
 	if (key == 27) {
 		exit(0);
+	} else if (key == 'w') {
+		coords[2] += 1;
+	} else if (key == 'a') {
+		coords[0] += 1;
+	} else if (key == 's') {
+		coords[2] -= 1;
+	} else if (key == 'd') {
+		coords[0] -= 1;
 	}
+	// std::cout << (int) key << " " << key << std::endl;
 }
-
-// GLvoid processSpecialKeys(int key, int x, int y) {
-// 	switch(key) {
-// 		default:
-// 			break;
-// 	}
-// }
 
 int main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
 	glutInitWindowSize(windowWidth, windowHeight);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("work - 1");
+	glutCreateWindow("work - 6");
+
+	glEnable(GL_DEPTH_TEST);
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glFrustum(LEFT, RIGHT, DOWN, UP, 1, 100);
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+	glEnable(GL_COLOR_MATERIAL);
+
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambient);
+
+	glTranslatef(0, 0, -20);
 
 	glutDisplayFunc(renderScene);
 	glutReshapeFunc(resizeScene);
-	// glutIdleFunc(renderScene);
+	glutIdleFunc(renderScene);
 
 	glutKeyboardFunc(processNormalKeys);
-	// glutSpecialFunc(processSpecialKeys);
 
 	glutMainLoop();
 
